656ca9ea102097b693686d545ed27757f85e763e
Radoslaw <radoslaw.kumorekit@gmail.com>
2018-06-03 12:56:29 +0200

    delete CMakeFiles from root dir

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/radoslaw/BuildSystems/Exercises/task3
-# Build directory: /home/radoslaw/BuildSystems/Exercises/task3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(palindromeLib)
-subdirs(palindromeTests)
