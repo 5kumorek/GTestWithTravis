5b59666cf0c5d5eaa96801497d9459f6a3898246
Radoslaw <radoslaw.kumorekit@gmail.com>
2018-06-03 12:53:47 +0200

    delete CMakeFiles

--- palindromeLib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/radoslaw/BuildSystems/Exercises/task3/palindromeLib
-# Build directory: /home/radoslaw/BuildSystems/Exercises/task3/palindromeLib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- palindromeTests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/radoslaw/BuildSystems/Exercises/task3/palindromeTests
-# Build directory: /home/radoslaw/BuildSystems/Exercises/task3/palindromeTests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(PalindromeTest "PalindromeTest")
